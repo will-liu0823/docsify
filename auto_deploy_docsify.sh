bc325b7043add7af0a4878c47b910b88b93baff8
will <will.liu0823@gmail.com>
2024-05-01 11:58:00 +0800

    update

--- auto_deploy_docsify.sh
+++ auto_deploy_docsify.sh
@@ -2,7 +2,7 @@
 # 删除之前部署的网站对应的目录
 rm -rf ./blog
 # 克隆仓库, 将会把项目所有内容克隆到当前目录的repo目录下(如果repo目录不存在会创建)
-git clone https://github.com/will-liu0823/docsify_blog.git blog
+git clone git@github.com:will-liu0823/docsify_blog.git blog
 # 将Dockerfile克隆到website目录的docs目录下
 cp ./Dockerfile ./blog/docs
 # 构建docker镜像
